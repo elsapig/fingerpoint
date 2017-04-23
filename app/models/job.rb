31b502c6a2cbbae088511698351ff2668888a105
dreamrobot <chingchih.wu@gmail.com>
2017-04-23 18:10:58 +0800

    creat submit cv link and resume model and link resume user and job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,22 +1,21 @@
 class Job < ApplicationRecord
+  has_many :resumes
+
   def publish!
    self.is_hidden = false
    self.save
- end
+  end
 
- def hide!
+  def hide!
    self.is_hidden = true
    self.save
- end
+  end
 
- scope :published, -> { where (is_hidden: false) }
- end
- 
- scope :recent, -> { order ('created_at DESC') }
- end
 
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
 end
