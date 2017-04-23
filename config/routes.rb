6d606ab0a9cc11785165333c675eb36c20af3cb7
dreamrobot <chingchih.wu@gmail.com>
2017-04-23 18:24:22 +0800

    create resume form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
-  root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
   namespace :admin do
     resources :jobs do
       member do
@@ -13,4 +9,12 @@ Rails.application.routes.draw do
       end
     end
   end
+end
+
+ resources :jobs do
+   resources :resumes
+ end
+
+
+ root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
