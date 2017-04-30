db5bc3bff43809e830658f1706d980ea4238aaf1
dreamrobot <chingchih.wu@gmail.com>
2017-05-01 04:30:02 +0800

    gem carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,11 @@ Rails.application.routes.draw do
       end
     end
   end
-end
 
- resources :jobs do
+
+  resources :jobs do
    resources :resumes
- end
+  end
 
-
- root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
