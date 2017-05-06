ce63d170eb9fd1d152fb5e9cda5d3397ab3b20dc
dreamrobot <chingchih.wu@gmail.com>
2017-05-07 00:31:18 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,7 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
-    end
-  end
 
-
-  resources :jobs do
    resources :resumes
   end
-
-  root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
